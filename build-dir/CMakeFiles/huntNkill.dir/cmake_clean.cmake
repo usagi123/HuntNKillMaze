98d14309d9ceaff3d7bcf760626d1cb103b05cb3
usagi123 <maipham.quanghuy@gmail.com>
2019-04-05 22:59:25 +0700

    Created build-dir

---
+++ build-dir/CMakeFiles/huntNkill.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/huntNkill.dir/main.cpp.o"
+  "huntNkill.pdb"
+  "huntNkill"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/huntNkill.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
